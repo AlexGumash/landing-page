264ab92ada6b644a451bd9a7002d6d0b137a98cd
AlexGumash <lesha.gumbin@yandex.ru>
2017-04-06 12:56:38 +0300

    carousel added

--- app/js/common.js
+++ app/js/common.js
@@ -1,19 +1,35 @@
 $(function() {
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
-})
-console.log('popa');
+	$('.owl-carousel').owlCarousel({
+		loop:true,
+		margin:10,
+		nav:false,
+		dotsData: true,
+		dotsContainer: '.owl-dots',
+		responsive:{
+				0:{
+						items:1
+				},
+				600:{
+						items:1
+				},
+				1000:{
+						items:1
+				}
+		}
+	})
+
+	owl = $('.owl-carousel').owlCarousel();
+
+
+	$(".prev").click(function () {
+			owl.trigger('prev.owl.carousel');
+	});
+
+	$(".next").click(function () {
+			owl.trigger('next.owl.carousel');
+	});
+
+	$('.owl-dot').click(function () {
+  		owl.trigger('to.owl.carousel', [$(this).index(), 300]);
+});
 });
