c8d51c26fccdb65dacac6453264c2b7384d89354
AlexGumash <lesha.gumbin@yandex.ru>
2017-04-09 16:42:04 +0300

    added some animation on pictures

--- app/js/common.js
+++ app/js/common.js
@@ -36,4 +36,45 @@ $(function() {
 		$('.menu--active').css('height', 'calc('+document.body.offsetHeight +  'px - 20vh )');
 	});
 
+	$('.down-part-list-item').on('mouseenter', function() {
+		var target = event.target;
+    while (target !== this) {
+        target = target.parentNode;
+      }
+
+    for (var i = 0; i < target.children.length; i++) {
+      var child = target.children[i];
+
+      if (child.classList.contains('image')) {
+        child.classList.add('image--active')
+        child.classList.remove('image--disactive')
+        child.children[0].classList.add('sub-image--active')
+        child.children[0].classList.remove('sub-image--disactive')
+      } else if (child.classList.contains('text-head')) {
+        child.classList.add('text-head--active')
+      }
+    }
+
+	});
+
+	$('.down-part-list-item').on('mouseleave', function() {
+    var target = event.target;
+    while (target !== this) {
+        target = target.parentNode;
+      }
+
+    for (var i = 0; i < target.children.length; i++) {
+      var child = target.children[i];
+
+      if (child.classList.contains('image')) {
+        child.classList.remove('image--active')
+        child.classList.add('image--disactive')
+        child.children[0].classList.remove('sub-image--active')
+        child.children[0].classList.add('sub-image--disactive')
+      } else if (child.classList.contains('text-head')) {
+        child.classList.remove('text-head--active')
+      }
+    }
+	});
+
 });
